836e1d4b5e1e287e258521c9a6766dd970650434
jonakuka <jonakuka123@gmail.com>
2024-12-24 20:49:00 +0100

    done

---
+++ DAY 31/main.js
@@ -0,0 +1,60 @@
+// do while loop
+
+var i =0;
+
+do{
+    i = i+1;
+    console.log(i);
+
+}while(i<5);
+
+console.log("--------------");
+
+var j = 0;
+while(j<5){
+    j =j +1;
+    console.log(j);
+}
+
+console.log("--------------");
+
+
+
+
+var students=["Jona", "Albina", "Vlera", "Uerds", "Arkena"];
+
+for (var e=0; e<students.length; e++){
+    console.log(students[e]);
+}
+
+console.log("--------------");
+
+var stu=["hi", "hello", "something", "yey", "no"];
+
+var y;
+
+ for(y of stu){
+    console.log(y);
+ }
+
+ console.log("--------------");
+
+ var teksti = "hi there how are you, urime festa";
+
+ var o = 0;
+
+ for(o of teksti){
+    console.log(o);
+ }
+
+ console.log("--------------");
+
+ var person = {firstName:"John" , lastName:"Doe", age:23,};
+
+ 
+
+ 
+
+
+
+ 
