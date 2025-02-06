f6150ce56a8c8559d2e3b9722d9b65b8627cd223
jonakuka <jonakuka123@gmail.com>
2025-02-06 20:18:22 +0100

    day 36

--- day 36/main.js
+++ day 36/main.js
@@ -20,7 +20,39 @@ $('#btn2').on("click", function(){
 $("#myDiv").click(function(){
     $(this).addClass("clicked");
 })
+.find('span')
+.attr('title', 'Hover over me');
 
 $("#btn3").click(function(){
+    // $(".hidden").show();
+    // $(".hidden").show(300);
+    $(".hidden").show('slow');
     
+})
+
+$("#btn4").click(function(){
+    $(".hidden").hide();
+})
+
+$("#square").click(function(){
+    $("#square").animate({
+        'width':'200px',
+        'height':'200px'
+    })
+})
+
+$(".testing").click(function(){
+    
+    $(".testing").animate({
+        'left':'+=50px',
+        'opacity':0.25,
+        'fontSize':'30px'
+    },300,
+
+    function(){
+        console.log("animation is completed");
+    }
+
+     
+    );
 })
