76059cc355d4912fa2d9a5f1e0cdb225b653091d
jonakuka <jonakuka123@gmail.com>
2025-02-11 20:48:19 +0100

    day 38

---
+++ day 38/main.js
@@ -0,0 +1,12 @@
+
+
+
+function submit(){ 
+const no = document.getElementById().value;
+const pass = document.getElementById().value;
+const hello= document.getElementById().value;
+console.log(no);
+console.log(pass);
+console.log(hello);
+
+}
